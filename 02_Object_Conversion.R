054b87fe65f4cc92efeb3ab02ff8170fd14f07c9
samuel-marsh <samuel.marsh@childrens.harvard.edu>
2022-09-26 11:22:02 -0400

    update conversion

--- 02_Object_Conversion.R
+++ 02_Object_Conversion.R
@@ -8,9 +8,9 @@ library(Seurat) #v4.1.0
 library(SingleCellExperiment) #v1.16.0
 library(qs) #v0.25.3
 
-hammond_main <- qread("~/Dropbox (BCH)/Hammond_2019_Immunity/Seurat_Objects/Hammond_et-al-2019_Seurat_Converted_v4.qs")
+hammond_main <- qread("data_objects/Hammond_et-al-2019_Seurat_Converted_v4.qs")
 
-hammond_aged <- qread("~/Dropbox (BCH)/Hammond_2019_Immunity/Seurat_Objects/Hammond_et-al-2019_Aged_Seurat_Converted_v4.qs")
+hammond_aged <- qread("data_objects/Hammond_et-al-2019_Aged_Seurat_Converted_v4.qs")
 
 # Convert & Save ------------------------------------------------------------------------------
 # Convert
@@ -18,8 +18,8 @@ hammond_main_sce <- as.SingleCellExperiment(x = hammond_main)
 hammond_aged_sce <- as.SingleCellExperiment(x = hammond_aged)
 
 # Save
-qsave(x = hammond_main_sce, file = "Hammond_et-al-2019_SCE_Converted_v1-16-0.qs")
-qsave(x = hammond_aged_sce, file = "Hammond_et-al-2019_Aged_SCE_Converted_v1-16-0.qs")
+qsave(x = hammond_main_sce, file = "data_objects/Hammond_et-al-2019_SCE_Converted_v1-16-0.qs")
+qsave(x = hammond_aged_sce, file = "data_objects/Hammond_et-al-2019_Aged_SCE_Converted_v1-16-0.qs")
 
 
 
@@ -33,16 +33,16 @@ library(Seurat) #v4.1.0
 library(SeuratDisk) #v0.0.0.9020
 library(qs) #v0.25.3
 
-hammond_main <- qread("~/Dropbox (BCH)/Hammond_2019_Immunity/Seurat_Objects/Hammond_et-al-2019_Seurat_Converted_v4.qs")
+hammond_main <- qread("data_objects/Hammond_et-al-2019_Seurat_Converted_v4.qs")
 
-hammond_aged <- qread("~/Dropbox (BCH)/Hammond_2019_Immunity/Seurat_Objects/Hammond_et-al-2019_Aged_Seurat_Converted_v4.qs")
+hammond_aged <- qread("data_objects/Hammond_et-al-2019_Aged_Seurat_Converted_v4.qs")
 
 
 # Convert Objects -----------------------------------------------------------------------------
 # Main Object
-SaveH5Seurat(object = hammond_main, filename = "Hammond_et-al-2019_anndata_Converted_v0-8-0.h5Seurat", verbose = T)
-Convert(source = "Hammond_et-al-2019_anndata_Converted_v0-8-0.h5Seurat", dest = "h5ad", verbose = T)
+SaveH5Seurat(object = hammond_main, filename = "data_objects/Hammond_et-al-2019_anndata_Converted_v0-8-0.h5Seurat", verbose = T)
+Convert(source = "data_objects/Hammond_et-al-2019_anndata_Converted_v0-8-0.h5Seurat", dest = "h5ad", verbose = T)
 
 # Aged Object
-SaveH5Seurat(object = hammond_aged, filename = "Hammond_et-al-2019_Aged_anndata_Converted_v0-8-0.h5Seurat", verbose = T)
-Convert(source = "Hammond_et-al-2019_Aged_anndata_Converted_v0-8-0.h5Seurat", dest = "h5ad", verbose = T)
+SaveH5Seurat(object = hammond_aged, filename = "data_objects/Hammond_et-al-2019_Aged_anndata_Converted_v0-8-0.h5Seurat", verbose = T)
+Convert(source = "data_objects/Hammond_et-al-2019_Aged_anndata_Converted_v0-8-0.h5Seurat", dest = "h5ad", verbose = T)
